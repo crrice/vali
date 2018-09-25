46834e7fb3837284a6abcd1a6780ad3bd9a62dde
Conor Rice <conor@addmi.com>
2018-09-25 10:48:00 -0600

    Recompile.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,20 +1,152 @@
-export declare const V: {
-    readonly string: VT;
-    readonly number: VT;
-    readonly boolean: VT;
-    literal(value: any): VT;
-    arrayOf(type: VFunc): VT;
-    oneOf(...types: VFunc[]): VT;
-    allOf(...types: VFunc[]): VT;
-    custom(func: VFunc): VT;
-    shape(obj: {
-        [k: string]: VFunc;
-    }): VT;
+declare const VBase: {
+    boolean: ((o: ModProxy<any>) => ModProxy<boolean> & SelfMap<DeMod<boolean, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    string: ((o: ModProxy<any>) => ModProxy<string> & SelfMap<DeMod<string, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    } & {
+        regex: (o: ModProxy<string>) => (rgx: RegExp) => ModProxy<string>;
+        email: (o: ModProxy<string>) => ModProxy<string>;
+        alphanumeric: (o: ModProxy<string>) => ModProxy<string>;
+        base64: (o: ModProxy<string>) => ModProxy<string>;
+        hex: (o: ModProxy<string>) => ModProxy<string>;
+        minLen: (o: ModProxy<string>) => (min: number) => ModProxy<string>;
+        maxLen: (o: ModProxy<string>) => (max: number) => ModProxy<string>;
+        isLen: (o: ModProxy<string>) => (len: number) => ModProxy<string>;
+    }>>) & {
+        m: {
+            regex: (o: ModProxy<string>) => (rgx: RegExp) => ModProxy<string>;
+            email: (o: ModProxy<string>) => ModProxy<string>;
+            alphanumeric: (o: ModProxy<string>) => ModProxy<string>;
+            base64: (o: ModProxy<string>) => ModProxy<string>;
+            hex: (o: ModProxy<string>) => ModProxy<string>;
+            minLen: (o: ModProxy<string>) => (min: number) => ModProxy<string>;
+            maxLen: (o: ModProxy<string>) => (max: number) => ModProxy<string>;
+            isLen: (o: ModProxy<string>) => (len: number) => ModProxy<string>;
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    number: ((o: ModProxy<any>) => ModProxy<number> & SelfMap<DeMod<number, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    } & {
+        integer: (o: ModProxy<number>) => ModProxy<number>;
+        max: (o: ModProxy<number>) => (max: number) => ModProxy<number>;
+        min: (o: ModProxy<number>) => (min: number) => ModProxy<number>;
+        lt: (o: ModProxy<number>) => (max: number) => ModProxy<number>;
+        gt: (o: ModProxy<number>) => (min: number) => ModProxy<number>;
+        interval: (o: ModProxy<number>) => (range: string) => ModProxy<number>;
+    }>>) & {
+        m: {
+            integer: (o: ModProxy<number>) => ModProxy<number>;
+            max: (o: ModProxy<number>) => (max: number) => ModProxy<number>;
+            min: (o: ModProxy<number>) => (min: number) => ModProxy<number>;
+            lt: (o: ModProxy<number>) => (max: number) => ModProxy<number>;
+            gt: (o: ModProxy<number>) => (min: number) => ModProxy<number>;
+            interval: (o: ModProxy<number>) => (range: string) => ModProxy<number>;
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    literal: (o: ModProxy<any>) => (<T extends Primitive>(lit: T) => ModProxy<T> & SelfMap<DeMod<T, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    arrayOf: (o: ModProxy<any>) => (<T>(type: ModProxy<T>) => ModProxy<T[]> & SelfMap<DeMod<T[], {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    } & {
+        minLen: <T>(o: ModProxy<T[]>) => (min: number) => ModProxy<T[]>;
+        maxLen: <T>(o: ModProxy<T[]>) => (max: number) => ModProxy<T[]>;
+        isLen: <T>(o: ModProxy<T[]>) => (len: number) => ModProxy<T[]>;
+    }>>) & {
+        m: {
+            minLen: <T>(o: ModProxy<T[]>) => (min: number) => ModProxy<T[]>;
+            maxLen: <T>(o: ModProxy<T[]>) => (max: number) => ModProxy<T[]>;
+            isLen: <T>(o: ModProxy<T[]>) => (len: number) => ModProxy<T[]>;
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    mapOf: (o: ModProxy<any>) => (<T>(type: ModProxy<T>) => ModProxy<{
+        [K: string]: T;
+    }> & SelfMap<DeMod<T, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    shape: (o: ModProxy<any>) => (<T>(shape: { [K in keyof T]: ModProxy<T[K]>; }) => ModProxy<T> & SelfMap<DeMod<T, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    } & {
+        noextra: <T>(o: ModProxy<T>) => ModProxy<T>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    oneOf: (o: ModProxy<any>) => (<T1, T2 = never, T3 = never, T4 = never, T5 = never>(types_0: ModProxy<T1>, types_1?: ModProxy<T2> | undefined, types_2?: ModProxy<T3> | undefined, types_3?: ModProxy<T4> | undefined, types_4?: ModProxy<T5> | undefined) => ModProxy<T1 | T2 | T3 | T4 | T5> & SelfMap<DeMod<T1 | T2 | T3 | T4 | T5, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    allOf: (o: ModProxy<any>) => (<T1, T2 = unknown, T3 = unknown, T4 = unknown, T5 = unknown>(types_0: ModProxy<T1>, types_1?: ModProxy<T2> | undefined, types_2?: ModProxy<T3> | undefined, types_3?: ModProxy<T4> | undefined, types_4?: ModProxy<T5> | undefined) => ModProxy<T1 & T2 & T3 & T4 & T5> & SelfMap<DeMod<T1 & T2 & T3 & T4 & T5, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
+    custom: (o: ModProxy<any>) => (<T = any>(fn: (v: unknown) => v is T) => ModProxy<T> & SelfMap<DeMod<T, {
+        optional: (o: ModProxy<any>) => ModProxy<any>;
+        custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+    }>>) & {
+        m: {
+            optional: (o: ModProxy<any>) => ModProxy<any>;
+            custom: (o: ModProxy<any>) => (fn: (v: any) => boolean) => ModProxy<any>;
+        };
+    };
 };
-declare type VFunc = (v: any) => boolean;
-declare type VT = {
-    (v: any): boolean;
-    optional: VT;
-    noextra: VT;
+declare const V: {
+    [K in keyof typeof VBase]: ReturnType<typeof VBase[K]>;
 };
-export {};
+declare type ModProxy<T> = {
+    (v: unknown): v is T;
+    f: ((v: T) => boolean)[];
+    e: Error[];
+};
+declare type ModSet<T> = {
+    [mod_name: string]: ((o: ModProxy<T>) => ModProxy<T>) | ((o: ModProxy<T>) => (...args: any[]) => ModProxy<T>);
+};
+declare type DeMod<S, T extends ModSet<S>> = {
+    [K in keyof T]: T[K] extends (o: ModProxy<any>) => ModProxy<S> ? ModProxy<S> : T[K] extends ((o: ModProxy<any>) => (...args: infer A) => ModProxy<S>) ? (...args: A) => ModProxy<S> : never;
+};
+declare type SelfMap<T> = {
+    [K in keyof T]: T[K] extends ModProxy<any> ? T[K] & SelfMap<T> : T[K] extends (...args: infer A) => ModProxy<infer R> ? (...args: A) => ModProxy<R> & SelfMap<T> : never;
+};
+declare type Primitive = undefined | null | string | number | boolean | {};
+export { V };
--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,178 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.V = {
-    get string() {
-        return makeV(v => typeof v === "string");
-    },
-    get number() {
-        return makeV(v => typeof v === "number");
-    },
-    get boolean() {
-        return makeV(v => typeof v === "boolean");
-    },
-    literal(value) {
-        return makeV(v => v === value);
-    },
-    arrayOf(type) {
-        return makeV(v => v instanceof Array && v.every(type));
-    },
-    oneOf(...types) {
-        return makeV(v => types.some(t => t(v)));
-    },
-    allOf(...types) {
-        return makeV(v => types.every(t => t(v)));
-    },
-    custom(func) {
-        return makeV(func);
-    },
-    shape(obj) {
-        const vfunc = (v) => v
-            && typeof v === "object"
-            && Object.entries(obj).every(([k, vdr]) => k in v
-                ? vdr(v[k])
-                : vdr["v_optional"])
-            && (vfunc["v_noextra"]
-                ? Object.keys(v).every(k => k in obj)
-                : true);
-        return makeV(vfunc);
-    }
-};
-function makeV(f) {
-    return Object.defineProperties(f, {
-        optional: {
-            get: function () { f["v_optional"] = true; return f; }
+function prox(f, mods) {
+    const me = new Proxy(f, {
+        get: function (o, p) {
+            return p in mods ? mods[p](me) : o[p];
         },
-        noextra: {
-            get: function () { f["v_noextra"] = true; return f; }
+        apply: function (t, _, args) {
+            t.e = [];
+            return t.f.every(f => f(args[0]));
         },
     });
+    return me;
 }
+function vErr(o, msg) {
+    o.e.push(new Error(msg));
+    return false;
+}
+const GLOBAL_MODS = {
+    optional: (o) => (o["_optional"] = true, o),
+    custom: (o) => (fn) => (o.f.push(fn), o),
+};
+const NUMBER_MODS = {
+    integer: (o) => {
+        const check = (v) => Number.isInteger(v) || vErr(o, "Number is not an integer.");
+        return (o.f.push(check), o);
+    },
+    max: (o) => (max) => {
+        const check = (v) => v <= max || vErr(o, "Number is larger than maximum.");
+        return (o.f.push(check), o);
+    },
+    min: (o) => (min) => {
+        const check = (v) => v >= min || vErr(o, "Number is smaller than minimum.");
+        return (o.f.push(check), o);
+    },
+    lt: (o) => (max) => {
+        const check = (v) => v < max || vErr(o, "Number is not less than the maximum.");
+        return (o.f.push(check), o);
+    },
+    gt: (o) => (min) => {
+        const check = (v) => v > min || vErr(o, "Number is not greater than the minimum");
+        return (o.f.push(check), o);
+    },
+    interval: (o) => (range) => {
+        const rangeCheck = parseRange(range);
+        const check = (v) => rangeCheck(v) || vErr(o, `Number is not within specified range of ${range}.`);
+        return (o.f.push(check), o);
+    }
+};
+function parseRange(range) {
+    const range_parse = range.match(/^([[(])\s*(.*?),\s*(.*?)\s*([\])])$/);
+    if (!range_parse)
+        throw new Error("Invalid range expression.");
+    const range_vals = [+range_parse[2], +range_parse[3]];
+    if (range_vals.some(n => Number.isNaN(n)))
+        throw new Error("Invalid interval expression.");
+    if (range_vals[0] > range_vals[1])
+        throw new Error("Invalid interval expression (interval defines the empty set).");
+    return v => (range_parse[1] === "(" ? range_vals[0] < v : range_vals[0] <= v) &&
+        (range_parse[4] === ")" ? v < range_vals[1] : v <= range_vals[1]);
+}
+const STRING_MODS = {
+    regex: (o) => (rgx) => {
+        const check = (v) => rgx.test(v) || vErr(o, "String did not pass the specified RegExp.");
+        return (o.f.push(check), o);
+    },
+    email: (o) => {
+        const check = (v) => email_regex.test(v) || vErr(o, "String did not appear to be a valid email.");
+        return (o.f.push(check), o);
+    },
+    alphanumeric: (o) => {
+        const check = (v) => alphanum_regex.test(v) || vErr(o, "String did not appear to be alphanumeric.");
+        return (o.f.push(check), o);
+    },
+    base64: (o) => {
+        const check = (v) => base64_regex.test(v) || vErr(o, "String did not appear to be valid base64.");
+        return (o.f.push(check), o);
+    },
+    hex: (o) => {
+        const check = (v) => hex_regex.test(v) || vErr(o, "String did not appear to be valid hex.");
+        return (o.f.push(check), o);
+    },
+    minLen: (o) => (min) => {
+        const check = (v) => v.length >= min || vErr(o, "String length is smaller than the minimum.");
+        return (o.f.push(check), o);
+    },
+    maxLen: (o) => (max) => {
+        const check = (v) => v.length <= max || vErr(o, "String length is larger than the maximum.");
+        return (o.f.push(check), o);
+    },
+    isLen: (o) => (len) => {
+        const check = (v) => v.length === len || vErr(o, "String length is not equal to the specified length.");
+        return (o.f.push(check), o);
+    },
+};
+const email_regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const alphanum_regex = /^[a-zA-Z0-9]*$/i;
+const base64_regex = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
+const hex_regex = /^([abcdef0-9]{2})*$/i;
+const ARRAY_MODS = {
+    minLen: (o) => (min) => {
+        const check = (v) => v.length >= min || vErr(o, "Array length is smaller than the minimum.");
+        return (o.f.push(check), o);
+    },
+    maxLen: (o) => (max) => {
+        const check = (v) => v.length <= max || vErr(o, "Array length is larger than the maximum.");
+        return (o.f.push(check), o);
+    },
+    isLen: (o) => (len) => {
+        const check = (v) => v.length === len || vErr(o, "Array length is not equal to the specified length.");
+        return (o.f.push(check), o);
+    },
+};
+const createShapeMods = (shape_spec) => ({
+    noextra: (o) => {
+        const check = (v) => Object.keys(v).every(k => k in shape_spec) || vErr(o, "Additional keys found in an object which disallows unknown keys.");
+        return (o.f.push(check), o);
+    },
+});
+const VBase = {
+    boolean: Object.assign((o) => {
+        const check = (v) => typeof v === "boolean" || vErr(o, "Value is not a boolean.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    string: Object.assign((o) => {
+        const check = (v) => typeof v === "string" || vErr(o, "Value is not a string.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS, STRING_MODS) }),
+    number: Object.assign((o) => {
+        const check = (v) => typeof v === "number" || vErr(o, "Value is not a number.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS, NUMBER_MODS) }),
+    literal: (o) => Object.assign((lit) => {
+        const check = (v) => v === lit || vErr(o, "Value was not the specified literal.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    arrayOf: (o) => Object.assign((type) => {
+        const check = (v) => (v instanceof Array || vErr(o, "Value is not an array.")) && (v.every(e => type(e)) || vErr(o, "Array is not of correct type."));
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS, ARRAY_MODS) }),
+    mapOf: (o) => Object.assign((type) => {
+        const check = (v) => (v && typeof v === "object" || vErr(o, "Value is not an object.")) && (Object.values(v).every(e => type(e)) || vErr(o, "Object has (at least) one value of incorrect type."));
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    shape: (o) => Object.assign((shape) => {
+        const check = (v) => v && typeof v === "object" && Object.entries(shape).every(([k, mp]) => k in v ? mp(v[k]) : mp["_optional"]) || vErr(o, "Value was not of correct shape.");
+        return (o.f.push(check), o["m"] = Object.assign({}, o["m"], createShapeMods(shape)), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    oneOf: (o) => Object.assign((...types) => {
+        const check = (v) => types.some(fn => fn(v)) || vErr(o, "Value was none of the specified types.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    allOf: (o) => Object.assign((...types) => {
+        const check = (v) => types.every(fn => fn(v)) || vErr(o, "Value was not (at least) one of the specified types.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+    custom: (o) => Object.assign((fn) => {
+        const check = (v) => fn(v) || vErr(o, "Value failed a custom type check.");
+        return (o.f.push(check), o);
+    }, { m: Object.assign({}, GLOBAL_MODS) }),
+};
+const V = new Proxy(VBase, {
+    get: function (vobj, prop) {
+        if (!(prop in vobj))
+            return undefined;
+        const o = Object.assign(() => { }, { f: [], e: [] });
+        const vdr = vobj[prop](o);
+        return "m" in vobj[prop]
+            ? prox(vdr, vobj[prop].m)
+            : (...args) => prox(vdr(...args), vdr["m"]);
+    },
+    set: function (vobj, prop, val) {
+        if (prop !== "m")
+            return false;
+        return (vobj[prop] = val, true);
+    },
+});
+exports.V = V;
